32331c618434552df08997bb3f4c58031e37ab96
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-03-29 16:08:48 -0300

    ALterando main

--- src/main/TesteGrafo.java
+++ src/main/TesteGrafo.java
@@ -3,13 +3,13 @@ package main;
 import java.util.List;
 
 import main.java.grafo.core.Grafo;
+import main.java.grafo.core.Vertice;
 import main.java.grafo.search.BuscaEmLargura;
 import main.java.grafo.search.BuscaEmProfundidade;
 
 public class TesteGrafo {
 	public static void main(String[] args) throws Exception {
-		
-		
+
 		// Criando um novo grafo
 		Grafo grafo = new Grafo();
 		grafo.adicionarVertice("A");
@@ -21,8 +21,9 @@ public class TesteGrafo {
 		grafo.adicionarVertice("G");
 		grafo.adicionarVertice("H");
 		grafo.adicionarVertice("I");
-		grafo.conectarVertices("A","B");
-		grafo.conectarVertices("A","C");grafo.conectarVertices("A","D");
+		grafo.conectarVertices("A", "B");
+		grafo.conectarVertices("A", "C");
+		grafo.conectarVertices("A", "D");
 		grafo.conectarVertices("B", "F");
 		grafo.conectarVertices("B", "I");
 		grafo.conectarVertices("D", "E");
@@ -33,17 +34,34 @@ public class TesteGrafo {
 		grafo.conectarVertices("I", "C");
 		grafo.conectarVertices("I", "H");
 		grafo.conectarVertices("E", "A");
-		List<String> caminho = BuscaEmProfundidade.getInstance().buscar(grafo,
-		"D", "H");
+		List<String> caminho = BuscaEmProfundidade.getInstance().buscar(grafo, "D", "H");
 		System.out.print("Caminho feito por uma busca em profundidade: ");
-		for(String passo : caminho) {
-		System.out.print(passo + " ");
+		for (String passo : caminho) {
+			System.out.print(passo + " ");
 		}
+
 		caminho = BuscaEmLargura.getInstance().buscar(grafo, "B", "G");
 		System.out.println();
 		System.out.print("Caminho feito por uma busca em largura: ");
-		for(String passo : caminho) {
-		System.out.print(passo + " ");
+
+		for (String passo : caminho) {
+			System.out.print(passo + " ");
 		}
+
+		Grafo arvore = grafo.arvoreGeradoraPorProfundidade();
+		System.out.println();
+		System.out.println("--- Árvore geradora ---");
+		System.out.println("Vértices");
+		
+		for (Vertice v : arvore.getVertices()) {
+			System.out.println("\t" + v.getRotulo());
+		}
+		System.out.println("Arestas");
+		for (Vertice v : arvore.getVertices()) {
+			for (Vertice adj : arvore.getAdjacencias(v.getRotulo())) {
+				System.out.println("\t" + v.getRotulo() + adj.getRotulo());
+			}
+		}
+
 	}
 }
--- src/main/java/grafo/core/Grafo.java
+++ src/main/java/grafo/core/Grafo.java
@@ -3,7 +3,6 @@ package main.java.grafo.core;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
@@ -22,7 +21,7 @@ public class Grafo {
 
 	public Grafo(int qtdVertices) {
 		if (qtdVertices <= 0) {
-			throw new IllegalArgumentException("A quantidade m�xima de v�rtices deve ser maior ou igual � 1");
+			throw new IllegalArgumentException("A quantidade máxima de vértices deve ser maior ou igual à 1");
 		}
 		qtdMaximaVertices = qtdVertices;
 		isQtdMaximaDefinida = true;
@@ -38,87 +37,89 @@ public class Grafo {
 			throw new Exception("A quantidade de vértices permitida (" + qtdMaximaVertices + ")	foi	excedida.");
 		}
 	}
-
-	public List<Vertice> getVertices() {
+	
+	public List<Vertice> getVertices(){
 		return this.vertices;
 	}
-
+	
 	public Vertice getVertice(String rotulo) {
 		this.existeVerticeOrThrow(rotulo);
 		int indice = this.rotulosEmIndices.get(rotulo);
 		return this.vertices.get(indice);
 	}
-
-	public void conectarVertices(String rotuloVerticeInicial, String rotuloVerticeFinal) throws Exception {
-		if (!this.existeVertice(rotuloVerticeInicial) || !this.existeVertice(rotuloVerticeFinal)) {
-			throw new Exception("Para adicionar uma aresta ambos os vértices devem existir.");
-		}
-		criarMatrizAdjacencia();
-		int indiceVerticeFinal = this.rotulosEmIndices.get(rotuloVerticeInicial);
+	
+	public void conectarVertices(String rotuloVerticeInicial, String rotuloVerticeFinal) throws Exception{
+		if(!this.existeVertice(rotuloVerticeInicial) || !this.existeVertice(rotuloVerticeFinal)) {
+    		throw new Exception("Para adicionar uma aresta ambos os vértices devem existir.");
+    	}
+	    criarMatrizAdjacencia();
+	    int indiceVerticeFinal = this.rotulosEmIndices.get(rotuloVerticeInicial);
 		int indiceVerticeInicial = this.rotulosEmIndices.get(rotuloVerticeFinal);
 		this.matrizAdjacencia.adicionarAresta(indiceVerticeInicial, indiceVerticeFinal);
 	}
-
+	
 	public List<Vertice> getAdjacencias(String vertice) {
 		this.existeVerticeOrThrow(vertice);
 		int indiceVertice = this.rotulosEmIndices.get(vertice);
 		return this.matrizAdjacencia.getAdjacencias(indiceVertice);
 	}
-
+	
 	private boolean existeVerticeOrThrow(String vertice) {
-		if (!existeVertice(vertice)) {
+		if(!existeVertice(vertice)) {
 			throw new IllegalArgumentException("O vértice não existe.");
 		}
 		return true;
 	}
-
+	
 	private boolean existeVertice(String rotuloVertice) {
-		int indice = this.rotulosEmIndices.get(rotuloVertice);
-		return this.vertices.get(indice) != null ? true : false;
+		return this.rotulosEmIndices.get(rotuloVertice) != null ? true : false;
 	}
 
 	private void criarMatrizAdjacencia() {
-		if (this.matrizAdjacencia == null) {
-			this.matrizAdjacencia = new MatrizAdjacencia(new ArrayList<Vertice>(this.vertices));
-		}
+	    if(this.matrizAdjacencia == null){
+	        this.matrizAdjacencia = new MatrizAdjacencia(new ArrayList<Vertice>(this.vertices));
+	    }
 	}
-
+	
 	public Grafo arvoreGeradoraPorProfundidade() throws Exception {
 		Grafo arvore = new Grafo();
 		Stack<Vertice> roloDeBarbante = new Stack<Vertice>();
 		LinkedHashSet<String> verticesVisitados = new LinkedHashSet<String>();
 		List<Vertice> vertices = getVertices();
-		for (Vertice v : vertices) {
+
+		for(Vertice v : vertices) {
 			arvore.adicionarVertice(v.getRotulo());
 		}
+		
 		Vertice verticePontoDePartida = vertices.get(0);
-		verticesVisitados.add(verticePontoDePartida.getRotulo());
-		roloDeBarbante.push(verticePontoDePartida);
-		while (!roloDeBarbante.empty()) {
-			Vertice verticeAnalisado = roloDeBarbante.peek();
-			Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
-			if (proximoVertice == null) {
-				roloDeBarbante.pop();
-			} else {
-				String rotulo = proximoVertice.getRotulo();
-				verticesVisitados.add(rotulo);
-				roloDeBarbante.push(proximoVertice);
-				arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo());
-			}
-		}
+        verticesVisitados.add(verticePontoDePartida.getRotulo());
+        roloDeBarbante.push(verticePontoDePartida);
+
+        while(!roloDeBarbante.empty()){
+        	Vertice verticeAnalisado = roloDeBarbante.peek();
+            Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
+            if(proximoVertice == null){
+                roloDeBarbante.pop();
+            } else {
+            	String rotulo = proximoVertice.getRotulo();
+            	verticesVisitados.add(rotulo);
+                roloDeBarbante.push(proximoVertice);
+                arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo());
+            }
+        }
+
 		return arvore;
 	}
-
-	private Vertice obterProximoVertice(Vertice vertice, LinkedHashSet<String> verticesVisitados) {
-		List<Vertice> adjacencias = getAdjacencias(vertice.getRotulo());
-		for (int i = 0; i < adjacencias.size(); i++) {
-			Vertice adjacencia = adjacencias.get(i);
-			boolean naoVisitadoAinda = !verticesVisitados.contains(adjacencia.getRotulo());
-			if (naoVisitadoAinda) {
-				return adjacencia;
-			}
-		}
-		return null;
-	}
-
+	
+	private Vertice obterProximoVertice(Vertice vertice, LinkedHashSet<String> verticesVisitados){
+    	List<Vertice> adjacencias = getAdjacencias(vertice.getRotulo());
+        for(int i=0; i<adjacencias.size(); i++){
+        	Vertice adjacencia = adjacencias.get(i);
+        	boolean naoVisitadoAinda = !verticesVisitados.contains(adjacencia.getRotulo());
+            if(naoVisitadoAinda){
+                return adjacencia;
+            }
+        }
+        return null;
+    }
 }
