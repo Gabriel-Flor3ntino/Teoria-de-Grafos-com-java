2cf09a0106983cb09a01aab793e2f957e1a115e6
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-03-29 00:02:34 -0300

    Implementando as classes Busca em Profundidade e Busca em Largura

--- src/main/TesteGrafo.java
+++ src/main/TesteGrafo.java
@@ -3,7 +3,8 @@ package main;
 import java.util.List;
 
 import main.java.grafo.core.Grafo;
-import main.java.grafo.core.Vertice;
+import main.java.grafo.search.BuscaEmLargura;
+import main.java.grafo.search.BuscaEmProfundidade;
 
 public class TesteGrafo {
 	public static void main(String[] args) throws Exception {
@@ -15,27 +16,34 @@ public class TesteGrafo {
 		grafo.adicionarVertice("B");
 		grafo.adicionarVertice("C");
 		grafo.adicionarVertice("D");
-		grafo.conectarVertices("A", "B");
-		grafo.conectarVertices("A", "C");
-		grafo.conectarVertices("A", "D");
-		
-		System.out.println("Grau do vértice A: " + grafo.getVertice("A").getGrau());
-		System.out.println("Grau do vértice D: " + grafo.getVertice("D").getGrau());
-		System.out.println("Grau do vértice C: " + grafo.getVertice("C").getGrau());
-		System.out.println();
-		
-		System.out.print("O vértice A possui as seguintes adjacências:");
-		List<Vertice> adjacentes = grafo.getAdjacencias("A");
-		for (Vertice vertice : adjacentes) {
-			System.out.print(vertice.getRotulo() + " ");
+		grafo.adicionarVertice("E");
+		grafo.adicionarVertice("F");
+		grafo.adicionarVertice("G");
+		grafo.adicionarVertice("H");
+		grafo.adicionarVertice("I");
+		grafo.conectarVertices("A","B");
+		grafo.conectarVertices("A","C");grafo.conectarVertices("A","D");
+		grafo.conectarVertices("B", "F");
+		grafo.conectarVertices("B", "I");
+		grafo.conectarVertices("D", "E");
+		grafo.conectarVertices("D", "I");
+		grafo.conectarVertices("D", "G");
+		grafo.conectarVertices("I", "A");
+		grafo.conectarVertices("I", "D");
+		grafo.conectarVertices("I", "C");
+		grafo.conectarVertices("I", "H");
+		grafo.conectarVertices("E", "A");
+		List<String> caminho = BuscaEmProfundidade.getInstance().buscar(grafo,
+		"D", "H");
+		System.out.print("Caminho feito por uma busca em profundidade: ");
+		for(String passo : caminho) {
+		System.out.print(passo + " ");
 		}
-		
+		caminho = BuscaEmLargura.getInstance().buscar(grafo, "B", "G");
 		System.out.println();
-		System.out.print("O vértice C possui as seguintes adjacências");
-		adjacentes = grafo.getAdjacencias("C");
-		
-		for (Vertice vertice : adjacentes) {
-			System.out.print(vertice.getRotulo() + " ");
+		System.out.print("Caminho feito por uma busca em largura: ");
+		for(String passo : caminho) {
+		System.out.print(passo + " ");
 		}
 	}
 }
