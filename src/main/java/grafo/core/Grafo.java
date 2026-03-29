c5930f3aa9d30cf453e76c677a5528084634c135
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-03-29 16:02:24 -0300

    Criando médodo  na classe Grafo

--- src/main/java/grafo/core/Grafo.java
+++ src/main/java/grafo/core/Grafo.java
@@ -2,6 +2,7 @@ package main.java.grafo.core;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -81,38 +82,43 @@ public class Grafo {
 			this.matrizAdjacencia = new MatrizAdjacencia(new ArrayList<Vertice>(this.vertices));
 		}
 	}
-	
+
 	public Grafo arvoreGeradoraPorProfundidade() throws Exception {
 		Grafo arvore = new Grafo();
-		
 		Stack<Vertice> roloDeBarbante = new Stack<Vertice>();
-		LinkedList<String> verticesVisitados = new LinkedList<String>();
+		LinkedHashSet<String> verticesVisitados = new LinkedHashSet<String>();
 		List<Vertice> vertices = getVertices();
-		
 		for (Vertice v : vertices) {
 			arvore.adicionarVertice(v.getRotulo());
 		}
-		
 		Vertice verticePontoDePartida = vertices.get(0);
 		verticesVisitados.add(verticePontoDePartida.getRotulo());
 		roloDeBarbante.push(verticePontoDePartida);
-		
-		 while (!roloDeBarbante.empty()) {
-			 Vertice verticeAnalisado = roloDeBarbante.peek();
-			 Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
-			 if (proximoVertice == null) {
-				 roloDeBarbante.pop();
-			 } else {
-				 String rotulo = proximoVertice.getRotulo();
-				 verticesVisitados.add(rotulo);
-				 roloDeBarbante.push(proximoVertice);
-				 arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo());
-			 }
-		 }
-		
+		while (!roloDeBarbante.empty()) {
+			Vertice verticeAnalisado = roloDeBarbante.peek();
+			Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
+			if (proximoVertice == null) {
+				roloDeBarbante.pop();
+			} else {
+				String rotulo = proximoVertice.getRotulo();
+				verticesVisitados.add(rotulo);
+				roloDeBarbante.push(proximoVertice);
+				arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo());
+			}
+		}
 		return arvore;
 	}
-	
-	
-	
+
+	private Vertice obterProximoVertice(Vertice vertice, LinkedHashSet<String> verticesVisitados) {
+		List<Vertice> adjacencias = getAdjacencias(vertice.getRotulo());
+		for (int i = 0; i < adjacencias.size(); i++) {
+			Vertice adjacencia = adjacencias.get(i);
+			boolean naoVisitadoAinda = !verticesVisitados.contains(adjacencia.getRotulo());
+			if (naoVisitadoAinda) {
+				return adjacencia;
+			}
+		}
+		return null;
+	}
+
 }
