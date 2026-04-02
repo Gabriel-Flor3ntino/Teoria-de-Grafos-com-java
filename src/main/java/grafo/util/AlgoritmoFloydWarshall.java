b34473418b6f96cbfa0afdbd7f2d6bceb67ce4e4
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-04-02 15:46:43 -0300

    criação e implementação do algoritmo de Floyd-Warshall

--- src/main/java/grafo/util/AlgoritmoFloydWarshall.java
+++ src/main/java/grafo/util/AlgoritmoFloydWarshall.java
@@ -1,5 +1,64 @@
 package main.java.grafo.util;
 
-public class AlgoritmoFloydWarshall {
+import main.java.grafo.core.Digrafo;
+import main.java.grafo.core.Vertice;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlgoritmoFloydWarshall {
+    private static AlgoritmoFloydWarshall algoritmoFloydWarshall;
+
+    private AlgoritmoFloydWarshall(){}
+
+    public static AlgoritmoFloydWarshall getInstance(){
+        if(algoritmoFloydWarshall == null){
+            return new AlgoritmoFloydWarshall();
+        }
+        return algoritmoFloydWarshall;
+    }
+
+    public Map<String, Map<String, Info>> processar(Digrafo digrafo){
+        Map<String, Map<String, Info>> matriz = new HashMap<>();
+        for(Vertice u : digrafo.getVertices()){
+            Map<String, Info> linha = new HashMap();
+            matriz.put(u.getRotulo(), linha);
+            for(Vertice v : digrafo.getVertices()){
+                int peso = digrafo.getPeso(u.getRotulo(), v.getRotulo());
+                int valor = peso == 0 ? Integer.MAX_VALUE : peso;
+                Info info = new Info();
+                info.porQualVertice = v;
+                info.distancia = u.getRotulo().equalsIgnoreCase(v.getRotulo()) ? 0 : valor;
+                linha.put(v.getRotulo(), info);
+            }
+        }
+
+        for(Vertice k : digrafo.getVertices()){
+            Map<String, Info> linhaK = matriz.get(k.getRotulo());
+            for(Vertice u : digrafo.getVertices()){
+                Map<String, Info> linhaU = matriz.get(u.getRotulo());
+                Info uk = linhaU.get(k.getRotulo());
+                for(Vertice v : digrafo.getVertices()){
+                    Info kv = linhaK.get(v.getRotulo());
+                    /*
+                    Essa verificação é necessária pois operações que envolvam o valor Integer.MAX_VALUE resultam em valores negativos
+                    devido a extrapolação do valor que um dado do tipo inteiro pode assumir. */
+                    int soma = uk.distancia == Integer.MAX_VALUE || kv.distancia == Integer.MAX_VALUE ? Integer.MAX_VALUE : uk.distancia + kv.distancia;
+                    if(soma < linhaU.get(v.getRotulo()).distancia){
+                        Info info = new Info();
+                        info.porQualVertice = uk.porQualVertice;
+                        info.distancia = soma;
+                        linhaU.put(v.getRotulo(), info);
+                    }
+                }
+            }
+        }
+
+        return matriz;
+    }
+
+    public class Info {
+        public int distancia;
+        public Vertice porQualVertice;
+    }
 }
