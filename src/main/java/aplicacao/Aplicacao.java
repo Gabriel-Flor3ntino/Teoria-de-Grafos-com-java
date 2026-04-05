05fc5f2e6a7300145cb86a5ddeeb33471e7e1eb6
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-04-04 22:00:28 -0300

    Criação dos métodos  e  na classe Grafo

--- src/main/java/aplicacao/Aplicacao.java
+++ src/main/java/aplicacao/Aplicacao.java
@@ -1,10 +1,53 @@
 package main.java.aplicacao;
 
-import java.util.Scanner;
+import main.java.grafo.core.Digrafo;
+import main.java.grafo.core.Grafo;
 public class Aplicacao {
 	
 	public static void main(String[] args) {
+		Aplicacao app = new Aplicacao();
+		Grafo grafo = null;
 		
+		app.menu();
+		
+		while(true) {
+			System.out.println("Digite a opção desejada: ");
+			String opcao = app.ler().toUpperCase();			
+			switch (opcao) {
+				case "G":
+					grafo = app.novoGrafo();
+					break;
+				case "D":
+					grafo = app.novoGrafo();
+					break;
+				case "V":
+					app.novoVertice(grafo);
+					break;
+				case "A":
+					app.novaAresta(grafo);
+					break;
+				case "PE":
+					app.novaArestaPonderada(grafo);
+					break;
+				case "P":
+					
+					break;
+				case "L":
+					break;
+				case "AG":
+					break;
+				case "PR":
+					break;
+				case "DIJ":
+					break;
+				case "FL":
+					break;
+				case "VIZ":
+					break;
+				case "S":
+				default:
+			}
+		}
 	}
 	
 	
@@ -33,4 +76,44 @@ public class Aplicacao {
 	private String ler() {
 		return input.next();
 	}
+	
+	private Grafo novoGrafo() {
+		Grafo grafo = new Grafo();
+		System.out.println("Novo grafo criado.");
+		return grafo;
+	}
+	
+	private Digrafo novoDigrafo() {
+		Digrafo digrafo = new Digrafo();
+		System.out.println("novo dígrafo criado.");
+		return digrafo;
+	}
+	
+	private void novoVertice(Grafo grafo) throws Exception {
+		System.out.println("Defina o nome do vértice ? ");
+		String nome = ler();
+		grafo.adicionarVertice(nome);
+		System.out.println("Novo vértice " + nome + " criado.");
+	}
+	
+	private void novaAresta(Grafo grafo) throws Exception {
+		System.out.print("Qual o vértice de origem ? ");
+		String vOrigem = ler();
+		System.out.print("Qual o vértice de destino ? ");
+		String vDestino = ler();
+		grafo.conectarVertices(vOrigem, vDestino, null);
+		System.out.println("Nova aresta criada.");
+	}
+	
+	private void novaArestaPonderada(Grafo grafo) throws Exception {
+		System.out.print("Qual o vértice de origem ? ");
+		String vOrigem = ler();
+		System.out.print("Qual o vértice de destino ? ");
+		String vDestino = ler();
+		System.out.print("Qual o peso da aresta ? ");
+		String peso = ler();
+		grafo.conectarVertices(vOrigem, vDestino, Integer.valueOf(peso));
+		System.out.println("Nova aresta ponderada criada.");
+	}
+	
 }
--- src/main/java/grafo/core/Grafo.java
+++ src/main/java/grafo/core/Grafo.java
@@ -37,110 +37,154 @@ public class Grafo {
 			throw new Exception("A quantidade de vértices permitida (" + qtdMaximaVertices + ")	foi	excedida.");
 		}
 	}
-	
-	public List<Vertice> getVertices(){
+
+	public List<Vertice> getVertices() {
 		return this.vertices;
 	}
-	
+
 	public Vertice getVertice(String rotulo) {
 		this.existeVerticeOrThrow(rotulo);
 		int indice = this.rotulosEmIndices.get(rotulo);
 		return this.vertices.get(indice);
 	}
-	
-	public void conectarVertices(String rotuloVerticeInicial, String rotuloVerticeFinal, Integer peso) throws Exception{
-		if(!this.existeVertice(rotuloVerticeInicial) || !this.existeVertice(rotuloVerticeFinal)) {
-    		throw new Exception("Para adicionar uma aresta ambos os vértices devem existir.");
-    	}
-	    criarMatrizAdjacencia();
-	    int indiceVerticeFinal = this.rotulosEmIndices.get(rotuloVerticeInicial);
+
+	public void conectarVertices(String rotuloVerticeInicial, String rotuloVerticeFinal, Integer peso)
+			throws Exception {
+		if (!this.existeVertice(rotuloVerticeInicial) || !this.existeVertice(rotuloVerticeFinal)) {
+			throw new Exception("Para adicionar uma aresta ambos os vértices devem existir.");
+		}
+		criarMatrizAdjacencia();
+		int indiceVerticeFinal = this.rotulosEmIndices.get(rotuloVerticeInicial);
 		int indiceVerticeInicial = this.rotulosEmIndices.get(rotuloVerticeFinal);
 		this.matrizAdjacencia.adicionarAresta(indiceVerticeInicial, indiceVerticeFinal, peso);
 	}
-	
+
 	public List<Vertice> getAdjacencias(String vertice) {
 		this.existeVerticeOrThrow(vertice);
 		int indiceVertice = this.rotulosEmIndices.get(vertice);
 		return this.matrizAdjacencia.getAdjacencias(indiceVertice);
 	}
-	
+
 	private boolean existeVerticeOrThrow(String vertice) {
-		if(!existeVertice(vertice)) {
+		if (!existeVertice(vertice)) {
 			throw new IllegalArgumentException("O vértice não existe.");
 		}
 		return true;
 	}
-	
+
 	boolean existeVertice(String rotuloVertice) {
 		return this.rotulosEmIndices.get(rotuloVertice) != null ? true : false;
 	}
 
 	void criarMatrizAdjacencia() throws Exception {
-	    if(this.matrizAdjacencia == null){
-	        this.matrizAdjacencia = new MatrizAdjacencia(new ArrayList<Vertice>(this.vertices));
-	    } else {
-	    	int qtdVerticesNaMatriz = this.matrizAdjacencia.getQtdVertices();
-	    	if(this.vertices.size() != qtdVerticesNaMatriz) {
-	    		MatrizAdjacencia matrizAdjacenciaTemp = new MatrizAdjacencia(this.vertices);
-	    		this.matrizAdjacencia.copiaValoresPara(matrizAdjacenciaTemp);
-	    		this.matrizAdjacencia = matrizAdjacenciaTemp;
-	    	}
-	    }
+		if (this.matrizAdjacencia == null) {
+			this.matrizAdjacencia = new MatrizAdjacencia(new ArrayList<Vertice>(this.vertices));
+		} else {
+			int qtdVerticesNaMatriz = this.matrizAdjacencia.getQtdVertices();
+			if (this.vertices.size() != qtdVerticesNaMatriz) {
+				MatrizAdjacencia matrizAdjacenciaTemp = new MatrizAdjacencia(this.vertices);
+				this.matrizAdjacencia.copiaValoresPara(matrizAdjacenciaTemp);
+				this.matrizAdjacencia = matrizAdjacenciaTemp;
+			}
+		}
 	}
-	
+
 	public Grafo arvoreGeradoraPorProfundidade() throws Exception {
 		Grafo arvore = new Grafo();
 		Stack<Vertice> roloDeBarbante = new Stack<Vertice>();
 		LinkedHashSet<String> verticesVisitados = new LinkedHashSet<String>();
 		List<Vertice> vertices = getVertices();
 
-		for(Vertice v : vertices) {
+		for (Vertice v : vertices) {
 			arvore.adicionarVertice(v.getRotulo());
 		}
-		
-		Vertice verticePontoDePartida = vertices.get(0);
-        verticesVisitados.add(verticePontoDePartida.getRotulo());
-        roloDeBarbante.push(verticePontoDePartida);
 
-        while(!roloDeBarbante.empty()){
-        	Vertice verticeAnalisado = roloDeBarbante.peek();
-            Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
-            if(proximoVertice == null){
-                roloDeBarbante.pop();
-            } else {
-            	String rotulo = proximoVertice.getRotulo();
-            	verticesVisitados.add(rotulo);
-                roloDeBarbante.push(proximoVertice);
-                arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo(), null);
-            }
-        }
+		Vertice verticePontoDePartida = vertices.get(0);
+		verticesVisitados.add(verticePontoDePartida.getRotulo());
+		roloDeBarbante.push(verticePontoDePartida);
+
+		while (!roloDeBarbante.empty()) {
+			Vertice verticeAnalisado = roloDeBarbante.peek();
+			Vertice proximoVertice = obterProximoVertice(verticeAnalisado, verticesVisitados);
+			if (proximoVertice == null) {
+				roloDeBarbante.pop();
+			} else {
+				String rotulo = proximoVertice.getRotulo();
+				verticesVisitados.add(rotulo);
+				roloDeBarbante.push(proximoVertice);
+				arvore.conectarVertices(verticeAnalisado.getRotulo(), proximoVertice.getRotulo(), null);
+			}
+		}
 
 		return arvore;
 	}
-	
-	Vertice obterProximoVertice(Vertice vertice, LinkedHashSet<String> verticesVisitados){
-    	List<Vertice> adjacencias = getAdjacencias(vertice.getRotulo());
-        for(int i=0; i<adjacencias.size(); i++){
-        	Vertice adjacencia = adjacencias.get(i);
-        	boolean naoVisitadoAinda = !verticesVisitados.contains(adjacencia.getRotulo());
-            if(naoVisitadoAinda){
-                return adjacencia;
-            }
-        }
-        return null;
-    }
-	
-	Map<String, Integer> getRotulosEmIndices(){
+
+	Vertice obterProximoVertice(Vertice vertice, LinkedHashSet<String> verticesVisitados) {
+		List<Vertice> adjacencias = getAdjacencias(vertice.getRotulo());
+		for (int i = 0; i < adjacencias.size(); i++) {
+			Vertice adjacencia = adjacencias.get(i);
+			boolean naoVisitadoAinda = !verticesVisitados.contains(adjacencia.getRotulo());
+			if (naoVisitadoAinda) {
+				return adjacencia;
+			}
+		}
+		return null;
+	}
+
+	Map<String, Integer> getRotulosEmIndices() {
 		return rotulosEmIndices;
 	}
 
 	MatrizAdjacencia getMatrizAdjacencia() {
 		return matrizAdjacencia;
 	}
-	
+
 	public int getPeso(String rotuloVerticeInicial, String rotuloVerticeFinal) {
 		int indiceVerticeInicial = rotulosEmIndices.get(rotuloVerticeInicial);
 		int indiceVerticeFinal = rotulosEmIndices.get(rotuloVerticeFinal);
 		return matrizAdjacencia.getPeso(indiceVerticeInicial, indiceVerticeFinal);
 	}
+
+	public String graphViz(boolean isGrafoPonderado) {
+		boolean isDigrafo = this instanceof Digrafo ? true : false;
+		StringBuilder graphViz = new StringBuilder();
+		graphViz.append("digraph D {\n");
+
+		for (Vertice v : getVertices()) {
+			String rotulo = v.getRotulo();
+			graphViz.append("\t").append(rotulo).append("[shape=circle]\n");
+			for (Vertice adj : getAdjacencias(rotulo)) {
+				String rotuloAdj = adj.getRotulo();
+				if (isDigrafo) {
+					graphVizConnection(isGrafoPonderado, isDigrafo, graphViz, rotulo, rotuloAdj);
+				} else {
+					String regex = ".*" + rotuloAdj + " -> " + rotulo + ".*";
+					boolean hasConexaoRedundante = !Pattern.compile(regex).matcher(graphViz.toString()).find();
+					if (hasConexaoRedundante) {
+						graphVizConnection(isGrafoPonderado, isDigrafo, graphViz, rotulo, rotuloAdj);
+					}
+				}
+			}
+		}
+
+		graphViz.append("}");
+		return graphViz.toString();
+	}
+
+	private void graphVizConnection(boolean isGrafoPonderado, boolean isDigrafo, StringBuilder graphViz, String rotulo,
+			String rotuloAdj) {
+
+		graphViz.append("\t\t").append(rotulo).append(" -> ").append(rotuloAdj).append("[");
+		if (!isDigrafo) {
+			graphViz.append("arrowhead=none");
+		}
+		if (!isDigrafo && isGrafoPonderado) {
+			graphViz.append(",");
+		}
+		if (isGrafoPonderado) {
+			graphViz.append("label=").append(getPeso(rotulo, rotuloAdj));
+		}
+		graphViz.append("]\n");
+	}
+
 }
